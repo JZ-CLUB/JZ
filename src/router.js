d5b43cc87eda15b34b85ee37d5463a22ecebfa0a
jingyoshen <406806652@qq.com>
2018-01-29 11:37:43 +0800

    活动详情

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import User from '@/view/user/index';
 import Cart from '@/view/cart/index';
 import Goods from '@/view/goods/index';
 import Home from '@/view/home/sss';
+import ActivityGoods from '@/view/ActivityGoods/index';
 
 Vue.use(Router);
 
@@ -39,6 +40,21 @@ const routes = [
     component: Goods,
     meta: {
       title: '商品详情'
+    }
+  },
+  {
+    path: '/activityGoods',
+    name: 'activityGoods',
+    component: ActivityGoods,
+    meta: {
+      title: '活动详情'
+    }
+  },
+  {
+    name: 'home',
+    component: Home,
+    meta: {
+      title: '首页'
     }
   }
 ];

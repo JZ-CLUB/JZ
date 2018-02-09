83097d28fe6291d69b8d9515c10b68836004d9eb
jingyoshen <406806652@qq.com>
2018-02-09 14:59:39 +0800

    活动详情代码

--- src/view/activityGoods/index.vue
+++ src/view/activityGoods/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="goods">
 
- 123
+    <div></div>
 
     <van-goods-action>
       <van-goods-action-big-btn primary @click="showShu">
@@ -53,6 +53,7 @@
 </template>
 
 <script>
+  import Vue from 'vue'
   import {
     Tag,
     Col,
@@ -94,11 +95,6 @@
         imgShow: false,
         price: 100,
         initialSku: {
-          price: '1.00', // 默认价格（单位元）
-          stock_num: 227, // 商品总库存
-          collection_id: 2270, // 无规格商品 skuId 取 collection_id，否则取所选 sku 组合对应的 id
-          none_sku: false, // 是否无规格商品
-          hide_stock: true // 是否隐藏剩余库存
         },
         goods: {
           title: '美国伽力果（约680g/3个）',
@@ -114,83 +110,21 @@
           // 所有sku规格类目与其值的从属关系，比如商品有颜色和尺码两大类规格，颜色下面又有红色和蓝色两个规格值。
           // 可以理解为一个商品可以有多个规格类目，一个规格类目下可以有多个规格值。
           tree: [
-            {
-              k: '门票类型', // skuKeyName：规格类目名称
-              v: [
-                {
-                  id: '1', // skuValueId：规格值 id
-                  name: '预售票', // skuValueName：规格值名
-                },
-                {
-                  id: '2',
-                  name: 'xxxx',
-                }
-              ],
-              k_s: 's1'
-              // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
-            },
-            {
-              k: '演出时间', // skuKeyName：规格类目名称
-              v: [
-                {
-                  id: '3', // skuValueId：规格值 id
-                  name: '2018年01月09日', // skuValueName：规格值名
-                },
-                {
-                  id: '4',
-                  name: '2018年01月10日',
-                }
-              ],
-              k_s: 's2'
-              // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
-            }
+
           ],
           // 所有 sku 的组合列表，比如红色、M 码为一个 sku 组合，红色、S 码为另一个组合
           list: [
-            {
-              id: 2259, // skuId，下单时后端需要
-              price: 100, // 价格（单位分）
-              s1: '1', // 规格类目 k_s 为 s1 的对应规格值 id
-              s2: '3', // 规格类目 k_s 为 s2 的对应规格值 id
-              s3: '0', // 最多包含3个规格值，为0表示不存在该规格
-              stock_num: 4 // 当前 sku 组合对应的库存
-            },
-            {
-              id: 2260, // skuId，下单时后端需要
-              price: 100, // 价格（单位分）
-              s1: '1', // 规格类目 k_s 为 s1 的对应规格值 id
-              s2: '4', // 规格类目 k_s 为 s2 的对应规格值 id
-              s3: '0', // 最多包含3个规格值，为0表示不存在该规格
-              stock_num: 3 // 当前 sku 组合对应的库存
-            },
-            {
-              id: 2261, // skuId，下单时后端需要
-              price: 100, // 价格（单位分）
-              s1: '2', // 规格类目 k_s 为 s1 的对应规格值 id
-              s2: '3', // 规格类目 k_s 为 s2 的对应规格值 id
-              s3: '0', // 最多包含3个规格值，为0表示不存在该规格
-              stock_num: 1 // 当前 sku 组合对应的库存
-            },
-            {
-              id: 2262, // skuId，下单时后端需要
-              price: 100, // 价格（单位分）
-              s1: '3', // 规格类目 k_s 为 s1 的对应规格值 id
-              s2: '4', // 规格类目 k_s 为 s2 的对应规格值 id
-              s3: '0', // 最多包含3个规格值，为0表示不存在该规格
-              stock_num: 10 // 当前 sku 组合对应的库存
-            }
           ],
+          nlist:[],
           price: '1.00', // 默认价格（单位元）
           stock_num: 227, // 商品总库存
           collection_id: 2270, // 无规格商品 skuId 取 collection_id，否则取所选 sku 组合对应的 id
           none_sku: false, // 是否无规格商品
           messages: [
           ],
-          hide_stock: true // 是否隐藏剩余库存
+          hide_stock: false // 是否隐藏剩余库存
         },
-
-        goods_con: {
-          // 商品标题
+        goods_con: {// 商品标题
           title: '测试商品',
           Img: 'https://img.yzcdn.cn/1.jpg'
         },
@@ -199,7 +133,13 @@
 
       };
     },
+    created: function() {
+      this.send(this);
 
+    },
+    computed:{
+
+    },
     methods: {
       formatPrice() {
         return '¥' + (this.goods.price / 100).toFixed(2);
@@ -209,9 +149,146 @@
       },
       handleBuyClicked(e){
         console.log(e)
+      },
+      send:function (e) {
+        //Ajax.post('target/goods/api/goodsdetail', {
+        Ajax.post('http://rap.taobao.org/mockjsdata/31603/get', {
+          goodsId: "129"
+        })
+          .then(function (response) {
+            //console.log(response)
+            if(response.statusText=="OK"){
+
+              let nData = response.data.data[0];
+              let oData = e.sku;
+              let i = 0;
+              let j = 0;
+              let o = 0;
+              e.goodsId = nData.goodsId;
+              e.goods.title = nData.goodsName;
+              e.price = nData.goodsStorePrice;
+              e.sku.stock_num = nData.goodsTotalStorage;
+              e.collection_id = nData.goodsId;
+              e.initialSku.price = nData.goodsStorePrice;
+              e.initialSku.collection_id = nData.goodsId;
+              e.initialSku.stock_num = nData.goodsTotalStorage;
+              console.log(nData);
+              for (let x in nData.specName){
+                let Data;
+                Data = {
+                  k:nData.specName[x],
+                  k_s:"s"+i,
+                  v:[]
+                };
+                oData.tree.push(Data);
+                i++;
+              }
+
+              for(let z in nData.goodsSpecValueAll){
+                let Data;
+                let goodsSpecValueAll = nData.goodsSpecValueAll[z];
+                for(let k in goodsSpecValueAll){
+                  Data = {
+                    id:goodsSpecValueAll[k].spValueId,
+                    name:goodsSpecValueAll[k].spValueName
+                  };
+                  oData.tree[j].v.push(Data);
+                }
+                j++;
+              }
+
+              for(let k in nData.goodsSpecList){
+                let Data;
+                let goodsSpecList = nData.goodsSpecList[k];
+                Data = {
+                  id: goodsSpecList.goodsSpecId, // skuId，下单时后端需要
+                  price: goodsSpecList.specGoodsPrice, // 价格（单位分）
+                  stock_num: goodsSpecList.specGoodsStorage // 当前 sku 组合对应的库存
+                };
+                oData.list.push(Data);
+              }
+
+              for(let k in nData.goodsSpecList){
+                let Data;
+                let goodsSpecList = nData.goodsSpecList[k];
+                let a = 0;
+                for(let z in goodsSpecList.specGoodsSpec){
+                  Data = "s"+a;
+                  oData.list[o]["s"+a]=z;
+                  a++;
+                }
+                o++;
+              }
+              console.log(oData);
+              /*数组长度*/
+              let len = 1;//数据总长度
+              let row = 0;
+              let span = 0;
+              let cc = 0;
+              let listData = {list:[]};
+              for(let k in oData.tree){
+                len = len * oData.tree[k].v.length;
+                span++;
+              }
+              function b(e) {
+                let spanlen = 1;
+                for(let z = span-1;z > e;z--){
+                  spanlen = spanlen * oData.tree[z].v.length;
+                }
+                console.log("----------------------");
+                if(e == span - 1){
+                  spanlen = 1;
+                }
+                cc = spanlen;
+                return spanlen;
+              }
+
+              let y = 0;
+              for (let z in oData.tree){
+                let  bb = b(row);//当前元素循环次数
+                let dd = oData.tree[z].v.length;//当前数组的个数
+                dd = len/(bb*dd);
+                console.log("s"+z);
+                let j = 0;
+                let p = 0;
+                for(let k in oData.tree[z].v){
+                  y = oData.tree[z].v.length;
+                  for(let a = 1; a <= dd; a++){
+                    for(let i = 1; i <= bb; i++){
+                      if(row == span - 1){
+                        if(p >= len){
+                          j++;
+                          p = j;
+                        }
+                        //listData.list[p];
+                        //oData.list[p]["s"+z]=oData.tree[z].v[k].id;
+                        console.log("当前数组index:"+p);
+                        console.log("属性id:"+oData.tree[z].v[k].id);
+                        p = p + y;
+                      }else{
+                        //oData.list[p]["s"+z]=oData.tree[z].v[k].id;
+                        console.log("当前数组index:"+p);
+                        console.log("属性id:"+oData.tree[z].v[k].id);
+                        p++;
+                      }
+                    }
+                  }
+                }
+                if(row<span){
+                  row++;
+                }
+              }
+            }
+
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       }
     }
   };
+
+
 </script>
 
 <style lang="less">
